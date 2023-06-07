9520b7e6706f434953caa02e9ff0bdd6465bcbcf
Bruno Luis <bruno.dev07@gmail.com>
2023-06-07 17:27:06 -0300

    first commit

--- src/service/productService.js
+++ src/service/productService.js
@@ -2,9 +2,9 @@ const { query } = require('../client/postgresql');
 
 const getProducts = async() => {
 
-    const queryText = 'SELECT * FROM products';
-    const result = await query(queryText);
-    return result.rows;
+    
+    return query('SELECT * FROM products')
+    
     
 };
 
